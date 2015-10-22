dd55144046fcd939555518399b8d1374c36fef35
unknown-user <projectttower@gmail.com>
2015-10-22 12:59:03 +1100

    Персонаж более не застревает в стенах.

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -4,11 +4,35 @@ using System.Collections;
 public class ButtonScript : MonoBehaviour {
 
 
-	void Start () {
+	private Collider2D col;
+
+	public GameObject door;
+
+	void Awake () 
+	{
+		col = gameObject.GetComponent<Collider2D> ();
 	
 	}
 
 	void Update () {
 	
 	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == Tags.character)
+		{
+			door.GetComponent<Collider2D>().enabled = false;
+		}		
+
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == Tags.character)
+		{
+			door.GetComponent<Collider2D>().enabled = true;
+		}
+	}
+
 }
--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class CharacterController : MonoBehaviour 
 {
-	private float speed=10f, jumpForce=800f;
+	private float speed=10f, jumpForce=600f;
 	private int number, direction=1;
 	private Rigidbody2D rigid;
-	private GameObject groundCheck, sight, wallCheck1, wallCheck2;
-	public bool grounded, wall=false;
+	private GameObject groundCheck, sight, wallCheck1, wallCheck2, wallCheck3;
+	private bool grounded, wall=false, wallAbove=false;
 	private float grRadius=0.3f;
 
 	public LayerMask whatIsGround;
@@ -20,6 +20,7 @@ public class CharacterController : MonoBehaviour
 		sight = transform.FindChild ("Sight").gameObject;
 		wallCheck1=transform.FindChild ("WallCheck1").gameObject;
 		wallCheck2=transform.FindChild ("WallCheck2").gameObject;
+		wallCheck3=transform.FindChild ("WallCheck3").gameObject;
 	}
 
 	public void FixedUpdate()
@@ -28,14 +29,24 @@ public class CharacterController : MonoBehaviour
 			rigid.velocity = Vector2.Lerp (rigid.velocity, new Vector2 (speed * direction, rigid.velocity.y), 2f);
 		else if (!wall)
 			rigid.velocity = new Vector2 (speed * direction, rigid.velocity.y);
-		else
-			rigid.velocity = new Vector2 (0f, rigid.velocity.y);
+		rigid.velocity = new Vector2 ((wall)? 0f: rigid.velocity.x, rigid.velocity.y);
+		wallAbove = Physics2D.OverlapCircle (wallCheck3.transform.position, grRadius, whatIsGround);
 		wall = (Physics2D.OverlapCircle (wallCheck1.transform.position, grRadius, whatIsGround)||
-		        Physics2D.OverlapCircle (wallCheck2.transform.position, grRadius, whatIsGround)||
+		        (Physics2D.OverlapCircle (wallCheck2.transform.position, grRadius, whatIsGround)&& !(wallAbove))||
 		        Physics2D.OverlapCircle (sight.transform.position, grRadius, whatIsGround));
 		grounded = Physics2D.OverlapCircle (groundCheck.transform.position, grRadius, whatIsGround);
+		if (Input.GetKeyDown (KeyCode.Space)&&(grounded))
+			rigid.AddForce (new Vector2 (0f, jumpForce));
 
 
+	}
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Cancel"))
+		{
+			Application.LoadLevel (Application.loadedLevel);
+		}
 
 	}
 
