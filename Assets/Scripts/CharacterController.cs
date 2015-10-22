4227c17713870ed40070cc8c44c9032b7700840b
TTTheMaTTT <projectttower@gmail.com>
2015-10-22 11:17:31 +1100

    first commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour 
+{
+	private float speed=10f, jumpForce=800f;
+	private int number, direction=1;
+	private Rigidbody2D rigid;
+	private GameObject groundCheck, sight, wallCheck1, wallCheck2;
+	public bool grounded, wall=false;
+	private float grRadius=0.3f;
+
+	public LayerMask whatIsGround;
+
+	public void Awake()
+	{
+
+		rigid = gameObject.GetComponent<Rigidbody2D> ();
+		groundCheck=transform.FindChild("GroundCheck").gameObject;
+		sight = transform.FindChild ("Sight").gameObject;
+		wallCheck1=transform.FindChild ("WallCheck1").gameObject;
+		wallCheck2=transform.FindChild ("WallCheck2").gameObject;
+	}
+
+	public void FixedUpdate()
+	{
+		if ((!wall) && (Mathf.Abs (rigid.velocity.x) <= speed - 1f))
+			rigid.velocity = Vector2.Lerp (rigid.velocity, new Vector2 (speed * direction, rigid.velocity.y), 2f);
+		else if (!wall)
+			rigid.velocity = new Vector2 (speed * direction, rigid.velocity.y);
+		else
+			rigid.velocity = new Vector2 (0f, rigid.velocity.y);
+		wall=(Physics2D.OverlapCircle (sight.transform.position, grRadius, whatIsGround));
+		grounded = Physics2D.OverlapCircle (groundCheck.transform.position, grRadius, whatIsGround);
+
+
+	}
+
+}
