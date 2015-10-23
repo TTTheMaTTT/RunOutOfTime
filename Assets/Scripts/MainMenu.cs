534da02078f71edf55d3bd4c56a2057cdd6a91f7
unknown-user <projectttower@gmail.com>
2015-10-24 04:01:05 +1100

    Добавил скрипт по работе с менюшкой и само меню. Научил игру переключаться между уровнями. Заготовка сделана. Теперь будем шлифовать.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject MainMenuUI;
+
+	public int levelNumber;
+
+	private GameObject activeWindow;
+
+
+	void Start()
+	{
+		activeWindow = MainMenuUI;
+		MainMenuUI.SetActive (true);
+		if (!PlayerPrefs.HasKey ("LastLevel")) {
+			PlayerPrefs.SetInt ("LastLevel", 1);
+			levelNumber = 1;
+			PlayerPrefs.SetString("LastLayerName","01");
+		} 
+		else
+			levelNumber=PlayerPrefs.GetInt("LastLevel");
+
+
+	}
+	
+	void Update	()
+	{
+
+	}
+	
+	public void StartGame()
+	{
+		if (levelNumber < 10) 
+		{
+			File.Delete(Application.dataPath + "/Saves/SavedDataLevel0"+levelNumber+".xml");
+			Application.LoadLevel ("Level0" + levelNumber);
+		}
+		else
+		{
+			File.Delete(Application.dataPath + "/Saves/SavedDataLevel"+levelNumber+".xml");
+			Application.LoadLevel ("Level" + levelNumber);
+		}
+	}
+	
+	public void LevelSelect()
+	{
+		File.Delete(Application.dataPath + "/Saves/SavedDataLevel01.xml");
+		Application.LoadLevel("Level01");
+	}
+
+	
+	public void Settings()
+	{
+	}
+}
