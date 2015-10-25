763c3b42def07a17765349a2accc47d6ed29d4ae
TTTheMaTTT <projectttower@gmail.com>
2015-10-25 12:57:34 +1100

    Когда же ты, бл***, отправишься?

--- Assets/Scripts/Tags.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Tags : MonoBehaviour {
-	
-	public const string character = "Character";
-	public const string controller = "GameController";
-
-}
